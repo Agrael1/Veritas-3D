6063e5deba56744701b8ab1fd40a7bfb52ce0311
Илья Дорошенко <agrael@live.ru>
2019-04-21 17:30:25 +0200

    Release (#3)
    * Several Updates to the core are coming
    New era of experiments: making code more readable and efficient
    Made a BitField STL, not safe but who cares anyway
    
    Signed-off-by: Agrael <Agrael@live.ru>
    
    * Here are Huge updates:
    Made: StringStream! This one does eesentially the same thing as string stream in C++
    Exception! Now throwable and readeable, soon will be try-catch blocks with different exceptions
    
    * Exceptions are real and they work properly!
    Added Window class, which represents screen and encapsulates work with console.
    
    Added VeritasEngine: it is a better version of olcGameEngine, but now it's capable only of making a Window.
    Namespaces are real in this framework. Now Veritas has its own C framework.

--- ConsoleApplication5/main.c
+++ ConsoleApplication5/main.c
@@ -1,23 +1,43 @@
-#include "BitStack.h"
 #include <stdio.h>
 #include "GameDemo.h"
 #include "VeritasEngine.h"
+#include "Exception.h"
 
-
-
-main()
+int main(void)
 {
-	struct GameDemo *GD = new(GameDemo);
+	struct VeritasEngine *VE;
+	try
+	{
+		VE = new(VeritasEngine);
+		VE->method->SetupScreen(VE, 320, 160, 8, 8);
+	}
+	catch (Exception, e)
+	{
+		MessageBoxA(NULL, e->method->what(e), e->method->GetType(), MB_OK | MB_ICONEXCLAMATION);
+	}
+	endtry
 
-	GD->_.method->ConstructConsole(GD, 320, 200, 4, 4);
-	GD->_.method->Start(GD);
+	if(VE)
+		delete(VE);
 
-	delete(GD);
+	return 0;
 
-	/*struct VeritasEngine *VE = new(VeritasEngine);
-	if(VE->method->ConstructConsole(VE, 1000, 1000, 8, 8))
-		VE->method->Start(VE);*/
+	/*struct GameDemo *GD;
+	try 
+	{
+		GD = new(GameDemo);
 
-	/*delete(VE);*/
+		if(GD->_.method->ConstructConsole(GD, 320, 200, 4, 4))
+			GD->_.method->Start(GD);
+	}
+	catch (Exception, e)
+	{
+		e->method->what(e);
+		printf("%s", e->whatBuffer);
+	}
+	endtry
+
+	if (GD)
+		delete(GD);*/
 
 }
